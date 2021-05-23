3a4ac4ed1d55168137530baee4f4210780c63582
alcik98 <alcik98@interia.pl>
2021-05-23 14:48:24 +0200

    alcik2

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,6 @@ public class Main {
         System.out.println("Push");
         System.out.println("Push2");
         System.out.println("ale beda rzeczy");
-        System.out.println("ale beda rzeczy2");
+        System.out.println("ale beda rzeczy3");
     }
 }
