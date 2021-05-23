d4ddf957ac47164e2e2bae3138a825d9d80f4e13
Barltos <karolb156@gmail.com>
2021-05-23 19:23:38 +0200

    Kodowanie Hamminga z pobieraniem wersji dziesietnej, wyswietlanie wejsciowego i kodowanego

--- src/Hamming.java
+++ src/Hamming.java
@@ -3,63 +3,63 @@ public class Hamming {
         return Math.log10(n)/Math.log10(2);
     }
 
-    public static void koduj_Hamming(String kod_wejsciowy)
-    {
-    int dlugosc = kod_wejsciowy.length(), dodatkowa_dlugosc=0, j=0, sprawdzam;
-    int[] tablica = new int[dlugosc];
-
+    public static void koduj_Hamming(int kod_wejsciowy) {
+        String wejscie=Integer.toBinaryString(kod_wejsciowy);
+        int dlugosc = wejscie.length(), dodatkowa_dlugosc = 0, j = 0, sprawdzam;
+        int[] tablica = new int[dlugosc];
+        System.out.println("Binarny kod wejsciowy: ");
         for (int i = 0; i < dlugosc; i++) {
-            tablica[i]=Character.getNumericValue(kod_wejsciowy.charAt(i));
+            tablica[i] = Character.getNumericValue(wejscie.charAt(i));
+            System.out.print(tablica[i]);
         }
 
-        for (int i=0; i<dlugosc; i++) {     //Sprawdzenie ile dodatkowych pozycji
-        if(log2(i+1) - (int) log2(i+1)==0)
-            dodatkowa_dlugosc++;
-    }
 
-    dlugosc += dodatkowa_dlugosc;
-        if(log2(dlugosc) - (int) log2(dlugosc)==0)  //dodaj 1 pozycje jezeli ostatnia pozycja to miejsce na kod
-    dlugosc++;
-
-    int[] kod = new int[dlugosc];
-
-        for (int i=0; i<dlugosc; i++) {     //Wstawiam 0 tam gdzie sa pozycje do policzenia
-        if(log2(i+1) - (int) log2(i+1)==0) {
-            kod[i] = 0;
+        for (int i = 0; i < dlugosc; i++) {     //Sprawdzenie ile dodatkowych pozycji
+            if (log2(i + 1) - (int) log2(i + 1) == 0)
+                dodatkowa_dlugosc++;
         }
-        else {
-            kod[i]=tablica[j];
-            j++;
-        }
-    }
 
-    j = 0;
-        for (int i=0; i<dlugosc; i++) {
-        if(log2(i+1) - (int) log2(i+1)==0) {
+        dlugosc += dodatkowa_dlugosc;
+        if (log2(dlugosc) - (int) log2(dlugosc) == 0)  //dodaj 1 pozycje jezeli ostatnia pozycja to miejsce na kod
+            dlugosc++;
 
-            int k=i;
-            while (k<dlugosc) {
-                sprawdzam = 0;
-                for(int l=0; l<i+1; l++) {
+        int[] kod = new int[dlugosc];
 
-                    if(k+l==dlugosc)
-                        sprawdzam=1;
-
-                    if ((log2(k + 1 + l) - (int) log2(k + 1 + l) != 0) && (sprawdzam==0)) {
-                        kod[i] += kod[k+l];
-                    }
-                }
-                k=k+(2*(i+1));
+        for (int i = 0; i < dlugosc; i++) {     //Wstawiam 0 tam gdzie sa pozycje do policzenia
+            if (log2(i + 1) - (int) log2(i + 1) == 0) {
+                kod[i] = 0;
+            } else {
+                kod[i] = tablica[j];
+                j++;
             }
-            kod[i] = kod[i]%2;
         }
-        else {
-            kod[i] = tablica[j];
-            j++;
-        }
-    }
 
-        for (int i=0; i<dlugosc; i++)
+        j = 0;
+        for (int i = 0; i < dlugosc; i++) {
+            if (log2(i + 1) - (int) log2(i + 1) == 0) {
+
+                int k = i;
+                while (k < dlugosc) {
+                    sprawdzam = 0;
+                    for (int l = 0; l < i + 1; l++) {
+
+                        if (k + l == dlugosc)
+                            sprawdzam = 1;
+
+                        if ((log2(k + 1 + l) - (int) log2(k + 1 + l) != 0) && (sprawdzam == 0)) {
+                            kod[i] += kod[k + l];
+                        }
+                    }
+                    k = k + (2 * (i + 1));
+                }
+                kod[i] = kod[i] % 2;
+            } else {
+                kod[i] = tablica[j];
+                j++;
+            }
+        }
+        System.out.println("\nKodowanie Hamminga: ");
+        for (int i = 0; i < dlugosc; i++)
             System.out.print(kod[i]);
     }
 }
