b70e11a878adb57a239e8e4c6e03c090af8f22ee
Badziohub <blazejbadzio@gmail.com>
2021-05-24 23:37:24 +0200

    ten pushuj5

--- src/Crc16.java
+++ src/Crc16.java
@@ -3,38 +3,37 @@ import java.util.Scanner;
 public class Crc16 { //to jest crc16 reverse
     static Scanner skaner = new Scanner(System.in);
     static void crc16(int input) {
-        int dataIn = input;
-        int poly = 0b1010000000000001;
+        long dataIn = input;
+        long poly = 0b1010000000000001;
         int reverse = 0b1111111111111111;
-        int wynik = reverse ^ dataIn;
+        long wynik = reverse ^ dataIn;
         char tem;
         int shiftcount = 1;
         while (shiftcount <= 8){
-            int dlugosc = Integer.toBinaryString(wynik).length();
-            tem = Integer.toBinaryString(wynik).charAt(dlugosc-1);
+            int dlugosc = Long.toBinaryString(wynik).length();
+            tem = Long.toBinaryString(wynik).charAt(dlugosc-1);
             wynik = wynik >>> 1;
             shiftcount++;
             if (tem == '1') {
                 wynik = wynik ^ poly;
             }
         }
-
-        int temp_licz=Integer.toBinaryString(wynik).length();
-        int licznikznakow=0;
+        long temp_licz=Long.toBinaryString(wynik).length();
+        long licznikznakow=0;
         String pierwszykod = "";
         String drugikod = "";
-        System.out.print("Wynik CRC16 = ");
+        System.out.print("\nWynik CRC16 modbus = ");
         while(16 - temp_licz > 0) {
             System.out.print("0");
             pierwszykod = pierwszykod+"0";
             temp_licz++;
             licznikznakow++;
             }
-        System.out.print(""+Integer.toBinaryString(wynik));
-        for(int i = 0; i < Integer.toBinaryString(wynik).length(); i++){
+        System.out.print(""+Long.toBinaryString(wynik));
+        for(int i = 0; i < Long.toBinaryString(wynik).length(); i++){
             if(i+licznikznakow >= 8){
-                drugikod = drugikod + Integer.toBinaryString(wynik).charAt(i);
-            } else pierwszykod = pierwszykod + Integer.toBinaryString(wynik).charAt(i);
+                drugikod = drugikod + Long.toBinaryString(wynik).charAt(i);
+            } else pierwszykod = pierwszykod + Long.toBinaryString(wynik).charAt(i);
         }
         System.out.println("\n Pierwszy kod = "+pierwszykod + "\n Drugikod = "+drugikod);
         }
--- src/Crc16normalne.java
+++ src/Crc16normalne.java
@@ -1,41 +1,43 @@
 import java.util.Scanner;
 
 public class Crc16normalne {
-    static Scanner skaner = new Scanner(System.in);
     static void crc16norm(int input) {
-        int dataIn = input;
-        int poly = 0b1000000000000101;
-        int reverse = 0b1111111111111111;
-        int wynik = reverse ^ dataIn;
-        char tem;
-        int shiftcount = 1;
-        while (shiftcount <= 8){
-            int dlugosc = Integer.toBinaryString(wynik).length();
-            tem = Integer.toBinaryString(wynik).charAt(dlugosc-1);
-            wynik = wynik >>> 1;
-            shiftcount++;
-            if (tem == '1') {
-                wynik = wynik ^ poly;
+        long dataIn = input;
+        long poly = 0b1100000001111;
+        long gdy_zero = 0;
+        String temp = Long.toBinaryString(dataIn);
+        temp += "000000000000";
+        dataIn = Long.parseLong(temp,2);
+        temp = Long.toBinaryString(dataIn);
+        int kres_petli = Long.toBinaryString(dataIn).length();
+        String iloraz="1";
+        String string_dataIn = "";
+        long kawalek_dataIn=0;
+        for (int j = 12; j < kres_petli-1;j++) {
+            if(j==12) {
+            for (int i = 0; i <= 12; i++) {
+                string_dataIn += Long.toBinaryString(dataIn).charAt(i);
             }
-        }
+            }
+                if (j >= 13) {
+                    string_dataIn = Long.toBinaryString(kawalek_dataIn);
+                    iloraz+="1";
+                    if(Long.toBinaryString(kawalek_dataIn).length() < 12){
+                        for (int k = 0; k <= 12-Long.toBinaryString(kawalek_dataIn).length();k++){
+                            string_dataIn += Long.toBinaryString(dataIn).charAt(j + 1 + k);
+                            iloraz+="0";
+                        }
+                        j = j + (12-Long.toBinaryString(kawalek_dataIn).length());
+                    } else if(Long.toBinaryString(kawalek_dataIn).length() == 12) string_dataIn += Long.toBinaryString(dataIn).charAt(j + 1);
 
-        int temp_licz=Integer.toBinaryString(wynik).length();
-        int licznikznakow=0;
-        String pierwszykod = "";
-        String drugikod = "";
-        System.out.print("\nWynik CRC16 = ");
-        while(16 - temp_licz > 0) {
-            System.out.print("0");
-            pierwszykod = pierwszykod+"0";
-            temp_licz++;
-            licznikznakow++;
+                }
+                kawalek_dataIn = Long.parseLong(string_dataIn, 2);
+                //System.out.println("\n kawalek data in po petli = " + Long.toBinaryString(kawalek_dataIn));
+                kawalek_dataIn = kawalek_dataIn ^ poly;
+                string_dataIn = Long.toBinaryString(kawalek_dataIn);
+                //System.out.println("\n  WYNIKI TEGO CZEGOS = " + Long.toBinaryString(kawalek_dataIn) + "\t J =" + j);
         }
-        System.out.print(""+Integer.toBinaryString(wynik));
-        for(int i = 0; i < Integer.toBinaryString(wynik).length(); i++){
-            if(i+licznikznakow >= 8){
-                drugikod = drugikod + Integer.toBinaryString(wynik).charAt(i);
-            } else pierwszykod = pierwszykod + Integer.toBinaryString(wynik).charAt(i);
-        }
-        System.out.println("\n Pierwszy kod = "+pierwszykod + "\n Drugikod = "+drugikod);
+        System.out.println("Iloraz ="+iloraz);
+        System.out.println("WYNIKI CRC12 = " + Long.toBinaryString(kawalek_dataIn));
     }
 }
