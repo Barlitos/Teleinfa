c07f47c58655274960b5c8c538048e56347ce358
Badziohub <blazejbadzio@gmail.com>
2021-06-01 17:15:31 +0200

    o ten jest git 1

--- src/Crc16.java
+++ src/Crc16.java
@@ -1,8 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Crc16 { //to jest crc16 reverse
-    static Scanner skaner = new Scanner(System.in);
+public class Crc16 { //to jest crc16, od tąd
     static long crc16(int input) {
         long dataIn = input;
         String wart_pocz = Long.toBinaryString(dataIn);
@@ -11,60 +10,41 @@ public class Crc16 { //to jest crc16 reverse
         temp += "0000000000000000";
         dataIn = Long.parseLong(temp,2);
         int kres_petli = Long.toBinaryString(dataIn).length();
-       // System.out.println("kres petli == " + kres_petli);
         String string_dataIn = "";
         long kawalek_dataIn=0;
         for (int j = 16; j <= kres_petli-1;j++) {
-          //  System.out.println("-------------------------------- \n \t J = "+j+"\n ---------------------------------");
             if(j==16) {
                 for (int i = 0; i <= 16; i++) {
                     string_dataIn += Long.toBinaryString(dataIn).charAt(i);
                 }
                 kawalek_dataIn = Long.parseLong(string_dataIn, 2);
-               // System.out.println("\n kawalek data in po petli = " + Long.toBinaryString(kawalek_dataIn));
                 kawalek_dataIn = kawalek_dataIn ^ poly;
                 string_dataIn = Long.toBinaryString(kawalek_dataIn);
-               // System.out.println("\n  WYNIKI TEGO CZEGOS = " + Long.toBinaryString(kawalek_dataIn) + "\t J =" + j);
             } else if (j >= 17) {
-               // System.out.println("Wszedł do if >= 13");
                 string_dataIn = Long.toBinaryString(kawalek_dataIn);
                 if(string_dataIn.length() < 16 ){
-                  //  System.out.println("Wszedł do if 2");
                     int temp_licz = 0;
-                   // System.out.println("12- Long.toBinaryString(kawalek_dataIn).length() =="+(17-Long.toBinaryString(kawalek_dataIn).length()));
-                    //System.out.println("13-string_dataIn.length() == "+(17-string_dataIn.length())+"  kres_petli-j=="+(kres_petli-j));
                     if(!(13-string_dataIn.length() > kres_petli-j)){
-                       // System.out.println("zwariowany if");
                         for (int k = 0; k <= 16-Long.toBinaryString(kawalek_dataIn).length();k++){
                             string_dataIn += Long.toBinaryString(dataIn).charAt(j + k);
                             temp_licz++;
                         }
-                       // System.out.println("Po pętli w if 13 stoimy na tym == "+string_dataIn);
                         j = j + temp_licz-1;
                         kawalek_dataIn = Long.parseLong(string_dataIn, 2);
-                       // System.out.println("\n kawalek data in po petli = " + Long.toBinaryString(kawalek_dataIn));
                         kawalek_dataIn = kawalek_dataIn ^ poly;
                         string_dataIn = Long.toBinaryString(kawalek_dataIn);
-                       // System.out.println("\n  WYNIKI TEGO CZEGOS = " + Long.toBinaryString(kawalek_dataIn) + "\t J =" + j);
                     } else if(17-string_dataIn.length() > kres_petli-j){
-                       // System.out.println("13-string_dataIn.length() == "+(17-string_dataIn.length())+"  kres_petli-j=="+(kres_petli-j));
-                       // System.out.println("Wszedł do else 1");
                         for (int k = 0; k < kres_petli - j;k++){
-                            //System.out.println("Pętla iks de nr 2, j+k = "+(j+k));
                             string_dataIn += Long.toBinaryString(dataIn).charAt(j + k);
                             temp_licz++;
                         }
                         j = j + temp_licz-1;
                     }
                 } else if(Long.toBinaryString(kawalek_dataIn).length() == 16){
-                   // System.out.println("Wszedł do else 2");
                     string_dataIn += Long.toBinaryString(dataIn).charAt(j - 1);
-                    //System.out.println("Wszedł do else i stoi na tym == "+string_dataIn);
                     kawalek_dataIn = Long.parseLong(string_dataIn, 2);
-                    //System.out.println("\n kawalek data in po petli = " + Long.toBinaryString(kawalek_dataIn));
                     kawalek_dataIn = kawalek_dataIn ^ poly;
                     string_dataIn = Long.toBinaryString(kawalek_dataIn);
-                    //System.out.println("\n  WYNIKI TEGO CZEGOS = " + Long.toBinaryString(kawalek_dataIn) + "\t J =" + j);
                 }
 
             }
@@ -79,70 +59,48 @@ public class Crc16 { //to jest crc16 reverse
 
             wart_pocz += zera;
         } else wart_pocz += string_dataIn;
-       // System.out.println("Dane wejściowe " + Integer.toBinaryString(input));
-       // System.out.println("WYNIKI CRC16 = " +wart_pocz+ string_dataIn);
-
         return Long.parseLong(wart_pocz,2);
         }
 
     static long crc16check(long input) {
         long dataIn = input;
         long poly = 0b11000000000000101;
-        int kres_petli = Long.toBinaryString(dataIn).length(); // dlugosc input
+        int kres_petli = Long.toBinaryString(dataIn).length();
         String string_dataIn = "";
-
         long kawalek_dataIn=0;
-        //System.out.println("wartosc pocz= "+Long.toBinaryString(dataIn) );
-        //System.out.println("dane wklepane pocz= "+Long.toBinaryString(dataIn) );
         for (int j = 16; j < kres_petli;j++) {
-            //System.out.println("-------------------------------- \n \t J = "+j+"\n ---------------------------------");
             if(j==16) {
                 for (int i = 0; i <= 16; i++) {
                     string_dataIn += Long.toBinaryString(dataIn).charAt(i);
                 }
                 kawalek_dataIn = Long.parseLong(string_dataIn, 2);
-                //System.out.println("\n kawalek data in po petli = " + Long.toBinaryString(kawalek_dataIn));
                 kawalek_dataIn = kawalek_dataIn ^ poly;
                 string_dataIn = Long.toBinaryString(kawalek_dataIn);
-                //System.out.println("\n  WYNIKI TEGO CZEGOS = " + Long.toBinaryString(kawalek_dataIn) + "\t J =" + j);
             } else if (j >= 17) {
                 string_dataIn = Long.toBinaryString(kawalek_dataIn);
                 if(string_dataIn.length() < 16) {
-                    //System.out.println("Wszedł do if");
                     int temp_licz = 0;
                     if (!(17 - string_dataIn.length() > kres_petli - j)) {
                         for (int k = 0; k <= 16 - Long.toBinaryString(kawalek_dataIn).length(); k++) {
-                           // System.out.println("Początek pętli w if, wartosc string data in = " + string_dataIn + " , a j+k = " + (j + k));
                             string_dataIn += Long.toBinaryString(dataIn).charAt(j + k);
-                           // System.out.println("Po dodaniu elementu nr " + k + " w if, wartosc string data in = " + string_dataIn);
                             temp_licz++;
                         }
-                        //System.out.println("Po pętli w if 13 stoimy na tym == " + string_dataIn);
                         j = j + temp_licz - 1;
                         kawalek_dataIn = Long.parseLong(string_dataIn, 2);
-                        //System.out.println("\n kawalek data in po petli = " + Long.toBinaryString(kawalek_dataIn));
                         kawalek_dataIn = kawalek_dataIn ^ poly;
                         string_dataIn = Long.toBinaryString(kawalek_dataIn);
-                        //System.out.println("\n  WYNIKI TEGO CZEGOS = " + Long.toBinaryString(kawalek_dataIn) + "\t J =" + j);
                     } else if (17 - string_dataIn.length() > kres_petli - j) {
-                        //System.out.println("Wszedł do else 1");
-                        //System.out.println("(j+12-Long.toBinaryString(kawalek_dataIn).length()) < kres_petli) ==" + (j + 16 - Long.toBinaryString(kawalek_dataIn).length()) + "a kres pętli == " + kres_petli);
                         for (int k = 0; k < kres_petli - j; k++) {
-                           // System.out.println("Pętla iks de nr 2");
                             string_dataIn += Long.toBinaryString(dataIn).charAt(j + k);
                             temp_licz++;
                         }
                         j += temp_licz - 1;
                     }
                 } else if(Long.toBinaryString(kawalek_dataIn).length() == 16){
-                    //System.out.println("Wszedł do else i wrzuca mu = "+ Long.toBinaryString(dataIn).charAt(j));
                     string_dataIn += Long.toBinaryString(dataIn).charAt(j);
-                   // System.out.println("Wszedł do else i stoi na tym == "+string_dataIn);
                     kawalek_dataIn = Long.parseLong(string_dataIn, 2);
-                   // System.out.println("\n kawalek data in po petli = " + Long.toBinaryString(kawalek_dataIn));
                     kawalek_dataIn = kawalek_dataIn ^ poly;
                     string_dataIn = Long.toBinaryString(kawalek_dataIn);
-                    //System.out.println("\n  WYNIKI TEGO CZEGOS = " + string_dataIn + "\t J =" + j);
                 }
             }
 
@@ -206,6 +164,6 @@ public class Crc16 { //to jest crc16 reverse
             }
 
         }
-    }
+    } // do tąd
 }
 
--- src/Crc16normalne.java
+++ src/Crc16normalne.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Crc16normalne { // to jednak jest crc12
+public class Crc16normalne { // to jednak jest crc12 od tad
      long crc12norm(int input) {
          long dataIn = input;
          String wart_pocz = Long.toBinaryString(dataIn);
@@ -59,7 +59,6 @@ public class Crc16normalne { // to jednak jest crc12
 
              wart_pocz += zera;
      } else wart_pocz += string_dataIn;
-         //System.out.println( "Dane wejściowe zakodowane Crc12 = "+wart_pocz);
          return Long.parseLong(wart_pocz,2);
     }
 
@@ -71,57 +70,39 @@ public class Crc16normalne { // to jednak jest crc12
         String string_dataIn = "";
 
         long kawalek_dataIn=0;
-       // System.out.println("wartosc pocz= "+Long.toBinaryString(dataIn) );
-       // System.out.println("dane wklepane pocz= "+Long.toBinaryString(dataIn) );
         for (int j = 12; j < kres_petli;j++) {
-           // System.out.println("-------------------------------- \n \t J = "+j+"\n ---------------------------------");
             if(j==12) {
                 for (int i = 0; i <= 12; i++) {
                     string_dataIn += Long.toBinaryString(dataIn).charAt(i);
                 }
                 kawalek_dataIn = Long.parseLong(string_dataIn, 2);
-                //System.out.println("\n kawalek data in po petli = " + Long.toBinaryString(kawalek_dataIn));
                 kawalek_dataIn = kawalek_dataIn ^ poly;
                 string_dataIn = Long.toBinaryString(kawalek_dataIn);
-                //System.out.println("\n  WYNIKI TEGO CZEGOS = " + Long.toBinaryString(kawalek_dataIn) + "\t J =" + j);
             } else if (j >= 13) {
                 string_dataIn = Long.toBinaryString(kawalek_dataIn);
                 if(string_dataIn.length() < 12) {
-                   // System.out.println("Wszedł do if");
                     int temp_licz = 0;
                     if (!(13 - string_dataIn.length() > kres_petli - j)) {
                         for (int k = 0; k <= 12 - Long.toBinaryString(kawalek_dataIn).length(); k++) {
-                           // System.out.println("Początek pętli w if, wartosc string data in = " + string_dataIn + " , a j+k = " + (j + k));
                             string_dataIn += Long.toBinaryString(dataIn).charAt(j + k);
-                           // System.out.println("Po dodaniu elementu nr " + k + " w if, wartosc string data in = " + string_dataIn);
                             temp_licz++;
                         }
-                        //System.out.println("Po pętli w if 13 stoimy na tym == " + string_dataIn);
                         j = j + temp_licz - 1;
                         kawalek_dataIn = Long.parseLong(string_dataIn, 2);
-                       // System.out.println("\n kawalek data in po petli = " + Long.toBinaryString(kawalek_dataIn));
                         kawalek_dataIn = kawalek_dataIn ^ poly;
                         string_dataIn = Long.toBinaryString(kawalek_dataIn);
-                        //System.out.println("\n  WYNIKI TEGO CZEGOS = " + Long.toBinaryString(kawalek_dataIn) + "\t J =" + j);
                     } else if (13 - string_dataIn.length() > kres_petli - j) {
-                        //System.out.println("Wszedł do else 1");
-                       // System.out.println("(j+12-Long.toBinaryString(kawalek_dataIn).length()) < kres_petli) ==" + (j + 12 - Long.toBinaryString(kawalek_dataIn).length()) + "a kres pętli == " + kres_petli);
                         for (int k = 0; k < kres_petli - j; k++) {
-                          //  System.out.println("Pętla iks de nr 2");
                             string_dataIn += Long.toBinaryString(dataIn).charAt(j + k);
                             temp_licz++;
                         }
                         j += temp_licz - 1;
                     }
                 } else if(Long.toBinaryString(kawalek_dataIn).length() == 12){
-                   // System.out.println("Wszedł do else i wrzuca mu = "+ Long.toBinaryString(dataIn).charAt(j));
                     string_dataIn += Long.toBinaryString(dataIn).charAt(j);
-                   // System.out.println("Wszedł do else i stoi na tym == "+string_dataIn);
                     kawalek_dataIn = Long.parseLong(string_dataIn, 2);
-                  //  System.out.println("\n kawalek data in po petli = " + Long.toBinaryString(kawalek_dataIn));
                     kawalek_dataIn = kawalek_dataIn ^ poly;
                     string_dataIn = Long.toBinaryString(kawalek_dataIn);
-                   // System.out.println("\n  WYNIKI TEGO CZEGOS = " + Long.toBinaryString(kawalek_dataIn) + "\t J =" + j);
                 }
 
             }
@@ -185,5 +166,5 @@ public class Crc16normalne { // to jednak jest crc12
             }
 
         }
-    }
+    } // do tad
 }
